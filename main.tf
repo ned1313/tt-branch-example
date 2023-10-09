a3a90cbd63f228a7f74266d0fc7605a8f1607487
ned1313 <ned.bellavance@gmail.com>
2023-10-09 16:14:53 -0400

    add github actions

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "azurerm" {
+  features {
+    
+  }
+}
+
+variable "prefix" {
+  type = string
+  description = "(Required) The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  type = string
+  description = "(Required) The Azure Region in which all resources in this example should be created."
+}
+
+variable "environment" {
+  type = string
+  description = "(Required) The environment in which all resources in this example should be created."
+}
+
+resource "azurerm_resource_group" "main" {
+  location = var.location
+  name     = "${var.prefix}-branch"
+
+  tags = {
+    environment = var.environment
+  }
+}
